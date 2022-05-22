494a74afe3e23f9724ed55e991cb20e60ab7edc5
NCSanto01 <78079809+NCSanto01@users.noreply.github.com>
2022-05-22 12:54:13 +0100

    Todos los endpoints + estética de portafolio

--- src/main/java/PAT/practicaFinal/controller/AccountController.java
+++ src/main/java/PAT/practicaFinal/controller/AccountController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,15 @@ public class AccountController {
     @GetMapping("/account/{userId}")
     public ResponseEntity<Iterable<AccountModel>> retrieveByUserId(@PathVariable String userId){
         return ResponseEntity.ok().body(service.retrieveByUserId(userId));
+    }
+
+    @GetMapping("/account/{userId}/{symbol}")
+    public ResponseEntity<Iterable<AccountModel>> retrieveByUserIdSymbol(@PathVariable String userId, @PathVariable String symbol){
+        return ResponseEntity.ok().body(service.retrieveByUserIdSymbol(userId, symbol));
+    }
+
+    @PutMapping("/account/update")
+    public ResponseEntity<AccountModel> update(@RequestBody AccountModel editAccount){
+        return ResponseEntity.ok().body(service.update(editAccount));
     }
 }
--- src/main/java/PAT/practicaFinal/controller/OrderController.java
+++ src/main/java/PAT/practicaFinal/controller/OrderController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +16,33 @@ import PAT.practicaFinal.service.OrderService;
 @RequestMapping("/api/v1")
 public class OrderController {
     
+    String symbol = "BTCUSDT";
+
     @Autowired
     private OrderService service;
 
     @GetMapping("/orders/{userId}")
     public ResponseEntity<Iterable<OrderModel>> retrieveByUserId(@PathVariable String userId){
         return ResponseEntity.ok().body(service.retrieveByUserId(userId));
+    }
+
+    @GetMapping("/orders/{userId}/{symbol}")
+    public ResponseEntity<Iterable<OrderModel>> retrieveByUserIdSymbol(@PathVariable String userId, @PathVariable String symbol){
+        return ResponseEntity.ok().body(service.retrieveByUserIdSymbol(userId, symbol));
+    }
+
+    @PostMapping("/orders/create")
+    public ResponseEntity<OrderModel> create(@RequestBody OrderModel newOrder){
+        return ResponseEntity.ok().body(service.create(newOrder));
+    }
+
+    @PostMapping("/symbol/set")
+    public void setSymbol(@RequestBody String symbol){
+        this.symbol = symbol;
+    }
+
+    @GetMapping("/symbol/get")
+    public String getSymbol(){
+        return this.symbol;
     }
 }
